b8bc5f5367719cc92affbe068d5816048faff1fa
Damian Amalraj <damian.amalraj@hotmail.com>
2023-10-18 02:08:23 +0200

    ✅ Done: working

--- Program.cs
+++ Program.cs
@@ -9,24 +9,36 @@
 
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
 
-            for (int i = 0; i < 5; i++)
+            static void CheckGuess(int RandomNumber)
             {
-                int Guess = Convert.ToInt32(Console.ReadLine());
 
-                if (Guess < RandomNumber)
+                for (int i = 0; i <= 4; i++)
                 {
-                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    int Guess = Convert.ToInt32(Console.ReadLine());
+                    if (Guess != RandomNumber && i is 4)
+                    {
+                        Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
+                        return;
+                    }
+                    else if (Guess < RandomNumber)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    }
+                    else if (Guess > RandomNumber)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för högt!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wohoo! Du klarade det!");
+                        return;
+                    }
+
                 }
-                else if (Guess > RandomNumber)
-                {
-                    Console.WriteLine("Tyvärr, du gissade för högt!");
-                }
-                else
-                {
-                    Console.WriteLine("Wohoo! Du klarade det!")
-                }
+                Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
             }
 
+            CheckGuess(RandomNumber);
         }
     }
 }
